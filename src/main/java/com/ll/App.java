19355d09a29bd405e1fa2d9903066fcfdf0617b1
N1Alice <kc700370@gmail.com>
2023-10-27 17:27:29 +0900

    1단계

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+
+    Scanner scanner = new Scanner(System.in);
+    void run() {
+        System.out.println("==명언 앱==");
+        System.out.print("명령) ");
+    }
+
+}
